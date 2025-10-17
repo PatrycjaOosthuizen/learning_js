c740147c2796f7ff639d9c5c7945b7655059768d
Patrycja Oosthuizen <oosthuizen.code@gmail.com>
2025-10-17 05:40:14 +0100

    destructuring assignment exercise

---
+++ exercises/destructuring_assignment.js
@@ -0,0 +1,16 @@
+const LOCAL_FORECAST = {
+  today: { min: 72, max: 83 },
+  tomorrow: { min: 73.3, max: 84.6 },
+};
+
+function getMaxOfTmrw(forecast) {
+  "use strict";
+  // Using nested destructuring to get tomorrow.max
+  const {
+    tomorrow: { max: maxOfTomorrow },
+  } = forecast;
+
+  return maxOfTomorrow;
+}
+
+console.log(getMaxOfTmrw(LOCAL_FORECAST)); // 84.6
