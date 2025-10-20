0031f15cfb44db5074f65d01b0f15c22cb11d230
Patrycja Oosthuizen <oosthuizen.code@gmail.com>
2025-10-20 13:58:53 +0100

    Stack Operations
    Implement stack operations (push, pop, peek) using an array.

--- exercises/destructuring_assignment.js
+++ exercises/destructuring_assignment.js
@@ -1,3 +1,5 @@
+// Nested object destructuring
+
 const LOCAL_FORECAST = {
   today: { min: 72, max: 83 },
   tomorrow: { min: 73.3, max: 84.6 },
---
+++ exercises/stack_operations.js
@@ -0,0 +1,23 @@
+let stack = [];
+
+function pushItem(stack, item) {
+  stack.push(item); // adds the item to the end of the array
+  return stack;
+}
+
+function popItem(stack) {
+  return stack.pop(); // removes the last element of the array.
+}
+
+function peek(stack) {
+  return stack[stack.length - 1]; // accesses the last element.
+}
+
+// Testing the stack operations
+console.log("Initial Stack:", stack);
+pushItem(stack, "A");
+pushItem(stack, "B");
+console.log("After Push:", stack);
+console.log("Peek:", peek(stack));
+console.log("Pop:", popItem(stack));
+console.log("Stack now:", stack);
